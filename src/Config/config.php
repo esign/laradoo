3f0531f875342ec01fc487451550b48a341205f7
Tijl Clottemans <tijl@esign.eu>
2022-04-21 16:01:11 +0200

    removed xmlrpc as requirement temp

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'api-suffix' => env('ODOO_API_SUFFIX', 'xmlrpc'),     // 'xmlrpc' from version 7.0 and earlier, 'xmlrpc/2' from version 8.0 and above.
+    'api-suffix' => 'xmlrpc',     // 'xmlrpc' from version 7.0 and earlier, 'xmlrpc/2' from version 8.0 and above.
 
     //Credentials
-    'host'       => env('ODOO_HOST', 'https://my-host.com'),  // should contain 'http://' or 'https://'
-    'db'         => env('ODOO_DB', 'my-db'),
-    'username'   => env('ODOO_USERNAME', 'my-username'),
-    'password'   => env('ODOO_PASSWORD', 'my-password'),
+    'host'       => 'https://my-host.com',  // should contain 'http://' or 'https://'
+    'db'         => 'my-db',
+    'username'   => 'my-username',
+    'password'   => 'my-password',
 ];
